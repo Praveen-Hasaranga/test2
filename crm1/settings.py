41cc2963c47042fb3622e257390fc65885e574ba
Praveen-Hasaranga <67354619+Praveen-Hasaranga@users.noreply.github.com>
2020-07-12 12:32:44 +0530

    Update settings.py

--- crm1/settings.py
+++ crm1/settings.py
@@ -74,16 +74,16 @@ WSGI_APPLICATION = 'crm1.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
- DATABASES = {
-     'default': {
-         'ENGINE': 'django.db.backends.postgresql',
-         'NAME': 'demo_1',
-         'USER': 'praveen',
-         'PASSWORD': '123django#',
-         'HOST': 'database-1.cc8j86mrn9gj.us-east-2.rds.amazonaws.com',
-         'PORT': '5432'
-     }
- }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'demo_1',
+        'USER': 'praveen',
+        'PASSWORD': '123django#',
+        'HOST': 'database-1.cc8j86mrn9gj.us-east-2.rds.amazonaws.com',
+        'PORT': '5432'
+    }
+}
 
 # DATABASES = {
 #    'default': {
