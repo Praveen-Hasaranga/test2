91171b6279f6698fc6d7a5d8ce2adb2c39a37144
Praveen-Hasaranga <67354619+Praveen-Hasaranga@users.noreply.github.com>
2020-07-12 12:29:49 +0530

    Update settings.py

--- crm1/settings.py
+++ crm1/settings.py
@@ -74,23 +74,23 @@ WSGI_APPLICATION = 'crm1.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'demo_1',
-#         'USER': 'praveen',
-#         'PASSWORD': '123django#',
-#         'HOST': 'database-1.cc8j86mrn9gj.us-east-2.rds.amazonaws.com',
-#         'PORT': '5432'
-#     }
-# }
-
-DATABASES = {
-    'default': {
-         'ENGINE': 'django.db.backends.sqlite3',
-         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+ DATABASES = {
+     'default': {
+         'ENGINE': 'django.db.backends.postgresql',
+         'NAME': 'demo_1',
+         'USER': 'praveen',
+         'PASSWORD': '123django#',
+         'HOST': 'database-1.cc8j86mrn9gj.us-east-2.rds.amazonaws.com',
+         'PORT': '5432'
      }
  }
+
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
