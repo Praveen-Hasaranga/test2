095372fdf1c46f5fa6f876eda606803475ab8c9d
Praveen-Hasaranga <67354619+Praveen-Hasaranga@users.noreply.github.com>
2020-07-13 10:21:52 +0530

    Add files via upload

--- crm1/settings.py
+++ crm1/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'gu#hpesbf(ib6-h$(k6_&5kf23^r-g=*d07_(t*0k0s(m#sn9h'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['praveen-crm1.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['praveen-crm1.herokuapp.com', '127.0.0.1', 'crm-new.herokuapp.com']
 
 # Application definition
 
@@ -74,23 +74,23 @@ WSGI_APPLICATION = 'crm1.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'demo_1',
-#         'USER': 'praveen',
-#         'PASSWORD': '123django#',
-#         'HOST': 'database-1.cc8j86mrn9gj.us-east-2.rds.amazonaws.com',
-#         'PORT': '5432'
-#     }
-# }
-
 DATABASES = {
     'default': {
-         'ENGINE': 'django.db.backends.sqlite3',
-         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-     }
- }
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'demo_1',
+        'USER': 'praveen',
+        'PASSWORD': '123django#',
+        'HOST': 'database-1.cc8j86mrn9gj.us-east-2.rds.amazonaws.com',
+        'PORT': '5432'
+    }
+}
+
+# DATABASES = {
+#     'default': {
+#          'ENGINE': 'django.db.backends.sqlite3',
+#          'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#      }
+#  }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
@@ -133,6 +133,8 @@ MEDIA_URL = '/images/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
